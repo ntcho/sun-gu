96e6a5bc053a9fd0616d09f7c02543d27ee3c3b2
Nathan Cho <ntcho13@gmail.com>
2024-05-09 10:12:24 -0400

    Update deprecated functions

--- lib/chat/actions.tsx
+++ lib/chat/actions.tsx
@@ -162,7 +162,7 @@ async function submitUserMessage(content: string) {
   ;(async () => {
     try {
       const result = await experimental_streamText({
-        model: google.generativeAI('models/gemini-1.0-pro-001'),
+        model: google('models/gemini-1.0-pro-001'),
         temperature: 0,
         tools: {
           listDestinations: {
